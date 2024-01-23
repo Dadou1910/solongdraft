d649287b2763c6c2be4c30ffa302682d60f97516
Dadou1910 <142090885+Dadou1910@users.noreply.github.com>
2024-01-23 13:06:42 +0100

    Add files via upload

---
+++ src/init.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abremont <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/20 13:49:38 by abremont          #+#    #+#             */
+/*   Updated: 2024/01/23 11:56:34 by abremont         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minilibx-linux/mlx.h"
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	void	*img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "I love the present <3");
+	mlx_loop(mlx);
+	img = mlx_new_image(mlx, 1920, 1080);
+}
